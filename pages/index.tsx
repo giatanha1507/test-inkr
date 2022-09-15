b0be36a6bdb6315f5096b1d5ce9ef3e0f9210b40
TITANCORP\tan.ha <tan.ha@titancorpvn.com>
2022-09-15 10:09:35 +0700

    update reponsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Col, Row } from 'antd'
+import Layout from 'antd/lib/layout/layout'
 import type { NextPage } from 'next'
 import About from '../components/About'
 import Comment from '../components/Comment'
@@ -8,26 +9,31 @@ import Pricing from '../components/Pricing'
 import Related from '../components/Related'
 import Title from '../components/Title'
 import { useNovel } from '../contexts/NovelContext'
+import styles from './index.module.css'
 
 const Home: NextPage = () => {
 	const { novel, setNovel } = useNovel()
 	console.log(novel)
 	return (
-		<div style={{ padding: '20px 176px' }}>
-			<Row gutter={[32, 0]}>
-				<Col span={16}>
-					<Title />
-					<Pricing />
-					<ListItem />
-				</Col>
-				<Col span={8}>
-					<About />
-					<Related />
-					<Comment />
-					<Info />
-				</Col>
-			</Row>
-		</div>
+		<Layout className={styles.container}>
+			<div style={{ 
+				// padding: '20px 176px'
+				}}>
+				<Row gutter={[32, 0]}>
+					<Col span={24} lg={15} xl={16}>
+						<Title />
+						<Pricing />
+						<ListItem />
+					</Col>
+					<Col span={24} lg={9} xl={8}>
+						<About />
+						<Related />
+						<Comment />
+						<Info />
+					</Col>
+				</Row>
+			</div>
+		</Layout>
 	)
 }
 
